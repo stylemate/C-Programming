8d5bcf5a56e2638963261acce09e513bf854e7b5
stylemate <orum93@naver.com>
2013-10-28 16:23:08 +0800

    optimized 32_1024.c student array

--- 32_1024.c
+++ 32_1024.c
@@ -21,15 +21,13 @@ void main()
 	printf("Type 2 Usage of Array\n");
 	
 	for (count=1;count < 6; count++)
-		{
+	{
 		printf("학생 %d번:", count);
 		scanf("%d", &stu[count-1]);
-		} 
-	for (count = 1; count <6; count++)
-		{
 		sum2+=stu[count-1];
-		avr=sum2/5;
-		}
+	}
+
+	avr=sum2/5;
 
 	printf("\n합은 %1.2f \n",sum2);
 	printf("평균은 %1.2f \n", avr);
